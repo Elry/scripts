f8a77447501e20a2e05ca9ebe69400e7dbc89a2f
Rafael_Garcia <pmsp.smti.dev@hotmail.com>
2020-10-16 15:27:47 -0300

    Added a specific process info viewr

--- powershell/processTool.ps1
+++ powershell/processTool.ps1
@@ -1,5 +1,7 @@
 $menuOptions = @{1 = "show processes"
-  2 = "show installed apps (slow)"}
+  2 = "show installed apps (slow)"
+  3 = "show specific process information"
+}
 $menuOptions
 
 $uInp = Read-Host "Chose the task to run: "
@@ -11,5 +13,9 @@ switch($uInp) {
   { $_ -eq 2 } {
     Get-CimInstance -ClassName Win32_Product;
     break }
+  { $_ -eq 3 }{
+    $pInp = Read-Host "type process name(e.g: chrome, powershell): "
+    Get-Process $pInp
+  }
   default { "Option not found" }
 }
